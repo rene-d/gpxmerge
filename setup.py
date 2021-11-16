698160c3142118ca2c6dab8c037e70da8b4e0b55
rene-d <rene-d@users.noreply.github.com>
2021-11-16 06:13:40 +0100

    v0.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gpxmerge",
-    version="0.0.2",
+    version="0.0.3",
     author="rene-d",
     author_email="rene.github@gmail.com",
     description="GPXMerge",
